cb43e495d898d81d7059a81774da1823702d02c0
lijunwei <lijunwei@udesk.cn>
2021-09-19 16:40:07 +0800

    Commit manually.

---
+++ ruby/spec/turnstile_spec.rb
@@ -0,0 +1,11 @@
+require "#{ENV['PWD']}/demo01/turnstile"
+
+RSpec.describe Turnstile do
+  it "should have initial state of `Locked`" do
+    expect(:Locked).to eq(Turnstile.new.aasm.current_state)
+  end
+  it "should be off" do
+    turnstile = Turnstile.new
+    expect(true).to eq(turnstile.Locked?)
+  end
+end
