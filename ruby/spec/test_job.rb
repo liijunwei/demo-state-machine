2e6079936cfd93735a80c86ea9eab201197f8fe4
lijunwei <lijunwei@udesk.cn>
2021-09-19 15:26:17 +0800

    Commit manually.

---
+++ ruby/spec/test_job.rb
@@ -0,0 +1,13 @@
+# require '../demo01/job'
+
+# rspec spec/test_job.rb
+
+RSpec.describe "xxx" do
+# RSpec.describe Job do
+  it "should be sleeping" do
+    # job01 = Job.new
+
+    expect(true).to eq(true)
+    # expect(true).to eq(job01.sleeping?)
+  end
+end
