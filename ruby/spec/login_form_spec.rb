7d071603871fd17af51fe619e68d8d1e66f25d98
lijunwei <lijunwei@udesk.cn>
2021-09-19 16:47:08 +0800

    Commit manually.

---
+++ ruby/spec/login_form_spec.rb
@@ -0,0 +1,11 @@
+require "#{ENV['PWD']}/demo01/login_form"
+
+RSpec.describe LoginForm do
+  it "should have initial state of `start`" do
+    expect(:start).to eq(LoginForm.new.aasm.current_state)
+  end
+  it "should be off" do
+    login_form = LoginForm.new
+    expect(true).to eq(login_form.start?)
+  end
+end
