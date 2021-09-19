163a719df16c2d6471c2c56b602b1ebb639154a8
lijunwei <lijunwei@udesk.cn>
2021-09-19 20:17:36 +0800

    Commit manually.

--- ruby/demo01/turnstile.rb
+++ ruby/demo01/turnstile.rb
@@ -1,5 +1,15 @@
 require 'aasm'
 
+# http://smc.sourceforge.net/slides/SMC_Tutorial.pdf
+#
+# Description of the normal operation of a
+# turnstile:
+# 1. Turnstile initially locked
+# 2. A coin or ticket is inserted (event)
+# 3. Triggers action: turnstile unlocks
+# 4. A person passes through turnstile (event)
+# 5. Triggers action: turnstile locks again
+
 class Turnstile
   include AASM
 
