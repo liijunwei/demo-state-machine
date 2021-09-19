f83b3910c16bfa20c72c41365ad6fdd557691c1f
lijunwei <lijunwei@udesk.cn>
2021-09-19 16:32:14 +0800

    Commit manually.

--- ruby/spec/switch_spec.rb
+++ ruby/spec/switch_spec.rb
@@ -5,6 +5,11 @@ RSpec.describe Switch do
     switch = Switch.new
     expect(true).to eq(switch.off?)
   end
+  it "should be off, and can not switch_off again" do
+    switch = Switch.new
+    expect(true).to eq(switch.off?)
+    expect {switch.switch_off}.to raise_error(AASM::InvalidTransition)
+  end
   it "should be on" do
     switch = Switch.new
     switch.switch_on
